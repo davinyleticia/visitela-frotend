7fee5f16222ce91d4062bc16aa7d1b4b0cf8dc6f
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-03-30 00:19:53 -0300

    ajust size

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
     <div className=" bg-white">
       <div className="flex flex-row items-center justify-center max-w-7xl py-40 mx-auto px-1">
         <div>
-          <h1 className="text-4xl font-bold mb-4">Venha aconchegar no nosso servidor, ter o seu espaço online</h1>
-          <p className="text-lg mb-8">
+          <h1 className="text-5xl font-bold mb-4">Venha aconchegar no nosso servidor, ter o seu espaço online</h1>
+          <p className="text-3xl mb-8">
           Se junta ao universo visitela. Crie o seu espaço online, com o seu nome, e partilhe com quem quiser.
           </p>
           <ShortLinkForm />
