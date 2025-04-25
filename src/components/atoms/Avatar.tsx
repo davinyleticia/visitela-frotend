3a2bbe72f0a0f7229f933d1be91402bd6f9aac0d
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-04-25 02:14:14 -0300

    add

--- src/components/atoms/Avatar.tsx
+++ src/components/atoms/Avatar.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 
 
 export const Avatar = ({ src, alt }: { src: string; alt: string }) => (
-    <Image width={50} height={50} src={src} alt={alt} className="rounded-full object-cover" />
+    <Image width={250} height={250} src={src} alt={alt} className="rounded-full object-cover" />
 );
