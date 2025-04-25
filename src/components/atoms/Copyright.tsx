2f9209e63aaed3d000e622754ef3c150e1447398
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-04-25 11:19:08 -0300

    add

--- src/components/atoms/Copyright.tsx
+++ src/components/atoms/Copyright.tsx
@@ -1,7 +1,7 @@
 export const Copyright: React.FC = () => (
   <div className="text-center sm:text-left space-y-1 py-4 px-2">
     <p className="text-gray-900 text-xs sm:text-sm">
-      Visitela - CNPJ: 55.375.402/0001-62
+      CNPJ: 55.375.402/0001-62
     </p>
     <p className="text-gray-900 text-xs sm:text-sm">
       © 2025 Todos os direitos reservados.
