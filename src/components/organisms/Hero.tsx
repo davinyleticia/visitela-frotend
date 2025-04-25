0625ac91b35d4e2ade3f1f97edd44fa561d39bb0
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-04-25 02:53:30 -0300

    ajust

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -17,10 +17,10 @@ export default function Hero() {
         {/* Texto e formulário */}
         <div className="text-center md:text-left md:w-1/2">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
-            Venha aconchegar no nosso servidor, ter o seu espaço online
+          Venha se aconchegar no nosso servidor e tenha seu espaço online, onde você pode crescer, vender e compartilhar!
           </h1>
           <p className="text-lg sm:text-xl md:text-2xl mb-8">
-            Se junta ao universo visitela, com o seu: nome, comércio, banda, influencer e partilhe com quem quiser.
+          Se junta ao universo visitela, com o seu: nome, comércio, banda, influencer e partilhe com quem quiser.
           </p>
           <ShortLinkForm handleCreate={handleCreate}/>
         </div>
