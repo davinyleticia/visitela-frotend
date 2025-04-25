804935ebc3c24ed94696e7aa38775734ef6e7445
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-04-25 11:17:10 -0300

    add cnpj

--- src/components/atoms/Copyright.tsx
+++ src/components/atoms/Copyright.tsx
@@ -1,7 +1,7 @@
 export const Copyright: React.FC = () => (
   <div className="text-center sm:text-left space-y-1 py-4 px-2">
     <p className="text-gray-900 text-xs sm:text-sm">
-      Visitela Soluções Online LTDA - CNPJ: 00.000.000/0000-00
+      Visitela - CNPJ: 55.375.402/0001-62
     </p>
     <p className="text-gray-900 text-xs sm:text-sm">
       © 2025 Todos os direitos reservados.
